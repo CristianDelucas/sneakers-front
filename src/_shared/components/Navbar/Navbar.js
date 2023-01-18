6ba14e0aa2f5f4fcc8cff68ac1ed1c628f78d2cf
CristianDelucas <crdelucasnoguero@gmail.com>
2023-01-18 11:55:39 +0100

    add Dimensions navbar responsive

--- src/_shared/components/Navbar/Navbar.js
+++ src/_shared/components/Navbar/Navbar.js
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 import Avatar from "../../../assets/images/image-avatar.png";
 import IconCart from "../icons/IconCart";
 import IconMenu from "../icons/IconMenu";
+import IconLogo from "../icons/IconLogo";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const [active, setActive] = useState("");
+  const [width, setWindowWidth] = useState(0);
+  const sidebarActive = () => {
+    if (!active) {
+      setActive("active");
+    } else {
+      setActive("");
+    }
+  };
 
-  const handleOnMenu = () => {
-    setVisible(!visible);
+  useEffect(() => {
+    updateDimensions();
+
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+    if (width > 768) {
+      setVisible(false);
+    }
   };
 
   return (
     <div className="g-header">
       <div className="g-header__nav">
-        <IconMenu onClick={handleOnMenu} />
+        <ul className="g-header__nav--logo ui-list">
+          <li className="icon-menu">
+            <IconMenu onClick={() => setVisible(true)} />
+          </li>
+          <li className="icon-logo">
+            <IconLogo />
+          </li>
+        </ul>
+
         <div
           className={
             visible ? `g-header__nav__menu active` : `g-header__nav__menu`
           }
         >
-          <ul onClick={() => setVisible(!visible)}>
+          <ul onClick={() => setVisible(false)}>
             <li>Collections</li>
             <li>Men</li>
             <li>Women</li>
@@ -29,8 +57,6 @@ const Navbar = () => {
             <li>Contact</li>
           </ul>
         </div>
-
-        <Logo />
       </div>
       <div className="g-header__avatar">
         <IconCart />
